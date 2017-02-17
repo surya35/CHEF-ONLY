56665ad48c1aee5798b3403985b60b47ce21d641
surya35 <suryakst15@gmail.com>
2017-02-17 14:30:01 +0530

    chef work

---
+++ chef-repo1/roles/dbserverrole.rb
@@ -0,0 +1,3 @@
+name "dbserverrole"
+description "db server role"
+run_list "recipe[webserver]", "recipe[webserver::firewall_config]","recipe[webserver::db_config]"
---
+++ chef-repo1/roles/webserverrole.rb
@@ -0,0 +1,3 @@
+name "webserverrole"
+description "web server role"
+run_list "recipe[webserver]", "recipe[webserver::firewall_config]","recipe[webserver::webuser]","recipe[webserver::webserver_config]"
